5d3895f39894bf0289e7f3fb0cdb1380333a6857
Cherzing <czlifetime@foxmail.com>
2024-08-08 15:50:26 +0800

    Update index.js

--- styles/index.js
+++ styles/index.js
@@ -8,7 +8,7 @@ export default function Home() {
       <Head>
         <title>Colnago</title>
         <meta name="description" content="write-hub" />
-        <link rel="icon" href="../public/favicon.ico" />
+        <link rel="icon" href="../public/Google.svg" />
       </Head>
 
       <main className={styles.main}>
